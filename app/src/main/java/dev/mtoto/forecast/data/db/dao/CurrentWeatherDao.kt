e12f2fd2e5a9568d686dfa50ff57b964782d09c5
mtotodev05 <mtotodev05@gmail.com>
2019-03-24 12:00:17 +0300

    Setup database classes

---
+++ app/src/main/java/dev/mtoto/forecast/data/db/dao/CurrentWeatherDao.kt
@@ -0,0 +1,23 @@
+package dev.mtoto.forecast.data.db.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import dev.mtoto.forecast.data.db.entity.CURRENT_WEATHER_ID
+import dev.mtoto.forecast.data.db.entity.CurrentWeatherEntry
+import dev.mtoto.forecast.data.db.unitlocalized.ImperialCurrentWeatherEntry
+import dev.mtoto.forecast.data.db.unitlocalized.MetricCurrentWeatherEntry
+
+@Dao
+interface CurrentWeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(weatherEntry: CurrentWeatherEntry)
+
+    @Query("select * from current_weather where id = ${CURRENT_WEATHER_ID}")
+    fun getWeatherMetric():LiveData<MetricCurrentWeatherEntry>
+
+    @Query("select * from current_weather where id = ${CURRENT_WEATHER_ID}")
+    fun getWeatherImperial():LiveData<ImperialCurrentWeatherEntry>
+}
