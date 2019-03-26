4e593c4b591f541d9be9f6e4fe940d9a2088e4d1
mtotodev05 <mtotodev05@gmail.com>
2019-03-26 17:48:33 +0300

    Weather Location Dao

--- app/src/main/java/dev/mtoto/forecast/data/network/response/CurrentWeatherResponse.kt
+++ app/src/main/java/dev/mtoto/forecast/data/network/response/CurrentWeatherResponse.kt
@@ -2,11 +2,11 @@ package dev.mtoto.forecast.data.network.response
 
 import com.google.gson.annotations.SerializedName
 import dev.mtoto.forecast.data.db.entity.CurrentWeatherEntry
-import dev.mtoto.forecast.data.db.entity.Location
+import dev.mtoto.forecast.data.db.entity.WeatherLocation
 
 data class CurrentWeatherResponse(
     @SerializedName("current")
     val currentWeatherEntry: CurrentWeatherEntry,
-    val location: Location
+    val location: WeatherLocation
 
 )
